5c5deec3e021b9d7fc263684f75cc83cd5717a36
Sanchit  Chaurasiya <Sanchitchaurasiya90@gmail.com>
2026-08-01 19:46:42 +0530

    chnge h

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Hero from './Components/hero'
 import ScratchCardSection from './Components/ScratchCardSection'
 import WeddingInvitationSection from './Components/WeddingInvitationSection'
@@ -12,9 +12,41 @@ import MusicPlayer from './Components/MusicPlayer'
 const App = () => {
   const [showVideo, setShowVideo] = useState(true)
   const [isVideoClosing, setIsVideoClosing] = useState(false)
+  const [showHero, setShowHero] = useState(false)
+  const [showScratch, setShowScratch] = useState(false)
+  const [showInvitation, setShowInvitation] = useState(false)
+  const [showCelebration, setShowCelebration] = useState(false)
+  const [showRegards, setShowRegards] = useState(false)
+  const [showFlowers, setShowFlowers] = useState(false)
 
   const handleVideoEnd = () => {
     setIsVideoClosing(true)
+    
+    // Start showing sections sequentially after video starts closing
+    setTimeout(() => {
+      setShowFlowers(true)
+    }, 200)
+    
+    setTimeout(() => {
+      setShowHero(true)
+    }, 400)
+    
+    setTimeout(() => {
+      setShowScratch(true)
+    }, 800)
+    
+    setTimeout(() => {
+      setShowInvitation(true)
+    }, 1200)
+    
+    setTimeout(() => {
+      setShowCelebration(true)
+    }, 1600)
+    
+    setTimeout(() => {
+      setShowRegards(true)
+    }, 2000)
+    
     setTimeout(() => {
       setShowVideo(false)
     }, 1200)
@@ -23,12 +55,12 @@ const App = () => {
   return (
     <div>
       {showVideo && <VideoIntro onVideoEnd={handleVideoEnd} isClosing={isVideoClosing} />}
-      {!showVideo && <FallingFlowers />}
-      {!showVideo && <Hero />}
-      {!showVideo && <ScratchCardSection />}
-      {!showVideo && <WeddingInvitationSection />}
-      {!showVideo && <WeddingCelebrationSection />}
-      {!showVideo && <WarmRegardsSection />}
+      {showFlowers && <div className="animate-fade-in"><FallingFlowers /></div>}
+      {showHero && <div className="animate-fade-in"><Hero /></div>}
+      {showScratch && <div className="animate-fade-in"><ScratchCardSection /></div>}
+      {showInvitation && <div className="animate-fade-in"><WeddingInvitationSection /></div>}
+      {showCelebration && <div className="animate-fade-in"><WeddingCelebrationSection /></div>}
+      {showRegards && <div className="animate-fade-in"><WarmRegardsSection /></div>}
       <MusicPlayer isOpened={!showVideo} />
     </div>
   )
